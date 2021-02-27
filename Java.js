ca8ac0d160869b9621b80e58695f4479573fa6cf
vBuneky <67710776+vBuneky@users.noreply.github.com>
2021-02-27 21:36:15 +0300

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,1 @@
+const soundbtn = document.querySelector(".play-sound-btn");let myAudio = document.querySelector("#audio");soundbtn.addEventListener("click",()=>{ myAudio.play(); //play the audio after click.});
